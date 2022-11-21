ee4b00dbc62c29a30d408f7ef0a00a92083624b9
Damis <adam.smehyl@seznam.cz>
2022-11-21 22:37:25 +0100

    SNS lookup snippet

--- hellosolana/sns.js
+++ hellosolana/sns.js
@@ -1,3 +1,14 @@
+/// sns.js 
+/// Authored by Damis
+
+/// This file contains a code snippet for performing an SNS lookup on the Solana blockchain
+
+/// The process explained:
+/// 1) Solana domain name has to be hashed
+/// 2) Solana TLD authority is asked for the account key for the given hashed name
+///    - this process has to perform a security check of the signing authority to eliminate man-in-the-middle attacks (similar to DNS spoofing)
+/// 3) At last we perform the retrieve action against the account key, as a response, we get the registry object
+
 const {
     getHashedName,
     getNameAccountKey,
@@ -15,5 +26,22 @@ const SOL_TLD_AUTHORITY = new PublicKey(
     "58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx"
 );
 
-const accountName = "damis.sol"
+const accountName = "adamis.sol";
 
+const getDomainKey = async(domain) => {
+    let hashedDomain = await getHashedName(domain);             
+    let inputDomainKey = await getNameAccountKey(
+        hashedDomain, // hashed name
+        undefined, // name class -> we don't need to specify it
+        SOL_TLD_AUTHORITY); // signing authority
+    return {inputDomainKey: inputDomainKey, hashedInputName: hashedDomain};
+};
+
+const main = async() => {
+    const connection = new Connection(clusterApiUrl("mainnet-beta"),"confirmed");
+    const {inputDomainKey} = await getDomainKey(accountName.replace(".sol", "")); // DomainKey
+    const registry = await NameRegistryState.retrieve(connection, inputDomainKey); // SNS retrieve
+    console.log(registry.owner.toBase58());
+}
+
+main();
