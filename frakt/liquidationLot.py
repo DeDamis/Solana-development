a4685fd2d5f61903ea67e8bb2b4910d72007a58f
Damis <adam.smehyl@seznam.cz>
2022-12-01 21:32:56 +0100

    added return value

--- frakt/liquidationLot.py
+++ frakt/liquidationLot.py
@@ -5,6 +5,7 @@ import json
 from solana.rpc.commitment import Confirmed
 from solana.publickey import PublicKey
 from solana.rpc.api import Client
+import pandas as pd
 
 """
 # data finder
@@ -26,60 +27,74 @@ def unpack_LiquidationLot(data):
     # start index
     i = 8
     loan = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
-    print('loan: '+str(loan))
+    #print('loan: '+str(loan))
     i += 32
     nftMint = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
-    print('nftMint: '+str(nftMint))
+    #print('nftMint: '+str(nftMint))
     i += 32
     vaultNftTokenAccount = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
-    print('vaultNftTokenAccount: '+str(vaultNftTokenAccount))
+    #print('vaultNftTokenAccount: '+str(vaultNftTokenAccount))
     i += 32
     lotNoFeesPrice = data[i:i+5]
     lotNoFeesPrice = lotNoFeesPrice[::-1]
     lotNoFeesPrice = lotNoFeesPrice.hex()
     i+=8
-    print('lotNoFeesPrice: '+str(lotNoFeesPrice))
+    #print('lotNoFeesPrice: '+str(lotNoFeesPrice))
     winningChanceInBasePoints = data[i:i+1]
     winningChanceInBasePoints = winningChanceInBasePoints[::-1]
     winningChanceInBasePoints = winningChanceInBasePoints.hex()
     i+=8
-    print('winningChanceInBasePoints: '+str(winningChanceInBasePoints))
+    #print('winningChanceInBasePoints: '+str(winningChanceInBasePoints))
     startedAt = data[i:i+4]
     startedAt = startedAt[::-1]
     startedAt = startedAt.hex()
     i+=8
-    print('startedAt: '+str(startedAt))
+    #print('startedAt: '+str(startedAt))
     endingAt = data[i:i+4]
     endingAt = endingAt[::-1]
     endingAt = endingAt.hex()
     i+=8
-    print('endingAt: '+str(endingAt))
+    #print('endingAt: '+str(endingAt))
     lotState = data[i:i+1]
     lotState = lotState[::-1]
     lotState = lotState.hex()
     i+=12
-    print('lotState: '+str(lotState))
+    #print('lotState: '+str(lotState))
+    #print('lotState: '+str(int(str(lotState))))
     # lotstate:
     #    0 : not active
     #    1 : active
     #    2 : redeemed
     #    3 : paid back
+    lotstate=["not active", "active", "redeemed", "paid back"]
     #idk
     ticketsCount = data[i:i+1]
     ticketsCount = ticketsCount[::-1]
     ticketsCount = ticketsCount.hex()
     i+=1
-    print('ticketsCount: '+str(ticketsCount))
+    #print('ticketsCount: '+str(ticketsCount))
     gracePeriod = data[i:i+3]
     gracePeriod = gracePeriod[::-1]
     gracePeriod = gracePeriod.hex()
     i+=8
-    print('gracePeriod: '+str(gracePeriod))
+    #print('gracePeriod: '+str(gracePeriod))
     graceFee = data[i:i+2]
     graceFee = graceFee[::-1]
     graceFee = graceFee.hex()
     i+=4
-    print('graceFee: '+str(graceFee))
+    #print('graceFee: '+str(graceFee))
+    df = pd.DataFrame({'nftMint':nftMint.decode(), 'lotState':lotstate[int(str(lotState))], 'ticketsCount':ticketsCount }, index=[0])
+    return df
+    
+def parseLiquidationLot(pub_key):
+    pk = PublicKey(pub_key)
+    connection = Client("https://api.mainnet-beta.solana.com")    
+    result = json.loads(connection.get_account_info(pk, Confirmed, encoding="base64").to_json())
+    data = base64.b64decode(result['result']['value']['data'][0])
+    unpacked = unpack_LiquidationLot(data)
+    unpacked['liquidationLot'] = pub_key
+    return unpacked
+
 
 if __name__ == "__main__":
     pk = PublicKey("DwQZXXtbN8azZmo8AEwrwXcr3zqyFaYr3SKCYMXf32fw")
