beadaf640650291810d5a10e6f86ea03eef47260
Damis <adam.smehyl@seznam.cz>
2022-12-03 16:24:53 +0100

    fixed a few exceptions

--- frakt/test.py
+++ frakt/test.py
@@ -4,6 +4,7 @@ import struct
 import json
 import asyncio
 import pandas as pd
+import time
 
 from solana.rpc.commitment import Confirmed
 from solana.publickey import PublicKey
@@ -24,55 +25,83 @@ async def main():
     #print(data[30]['meta']['logMessages'][1])
     putLoanToLiquidations = []
     liquidateNftToRaffles = []
+    liqNFTtoRaffles = 0
+    putNFTtoRaffles = 0
+    print("Detection:")
     for index, transaction in enumerate(data):
         if data[index]['meta']['err'] == None:
             message = data[index]['meta']['logMessages'][1]
             if message == "Program log: Instruction: PutLoanToLiquidationRaffles":
-                print("PutLoanToLiquidationRaffles detected!")
+                putNFTtoRaffles += 1
+                #print(str(putNFTtoRaffles)+"x: PutLoanToLiquidationRaffles detected!", end='\r')
                 putLoanToLiquidations.append(data[index])
             elif message == "Program log: Instruction: LiquidateNftToRaffles":
+                liqNFTtoRaffles += 1
                 liquidateNftToRaffles.append(data[index])
-                print("LiquidateNftToRaffles detected!")
+                #print(str(liqNFTtoRaffles)+"x: LiquidateNftToRaffles detected!", end='\r')
                 #print(transaction)
                 #print(str(index)+":"+transaction['meta']['logMessages'][1])
                 #print(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
                 #print(transaction['transaction']['message']['accountKeys'][12]['pubkey'])
+            print("LiquidateNftToRaffles: "+str(liqNFTtoRaffles)+" | "+"PutLoanToLiquidationRaffles: "+str(putNFTtoRaffles), end='\r')
+    print("")
     dfAll = pd.DataFrame()
     #print(putLoanToLiquidations[0])
     for index, transaction in enumerate(putLoanToLiquidations):
+        print("["+str(index+1)+"/"+str(putNFTtoRaffles)+"] Fetching info for \"PutLoanToLiquidationRaffles\"", end='\r')
         #print(str(index)+":"+transaction['meta']['logMessages'][1])
         #print(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
         #print(transaction['transaction']['message']['accountKeys'][9]['pubkey'])
-        df = get_NFT_metadata(transaction['transaction']['message']['accountKeys'][9]['pubkey'])
+        exceptThrown=True
+        df2 = parseLiquidationLot(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
+        time.sleep(0.5)
+        df = pd.DataFrame({"nftMint":"0"}, index=[0])
+        if df2['lotState'].to_string(index=False) == "active":
+            try:
+                df = get_NFT_metadata(transaction['transaction']['message']['accountKeys'][9]['pubkey'])
+                time.sleep(2.5)
+                exceptThrown=False
+            except:
+                pass
         #print(df)
         #print("")
-        df2 = parseLiquidationLot(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
         #print(df2)
         df3 = pd.merge(df, df2, how="inner", left_on='nftMint', right_on='nftMint')
         #print(df3)
-        dfAll = pd.concat([dfAll, df3], ignore_index=True, copy=True)
-    
+        if exceptThrown:
+            dfAll = pd.concat([dfAll, df2], ignore_index=True, copy=True)
+        else:
+            dfAll = pd.concat([dfAll, df3], ignore_index=True, copy=True)
+    print("")
     #print(liquidateNftToRaffles[2]['transaction'])
     #print("")
     for index, transaction in enumerate(liquidateNftToRaffles):
+        print("["+str(index+1)+"/"+str(liqNFTtoRaffles)+"] Fetching info for \"LiquidateNftToRaffles\"", end='\r')
         #print(transaction['transaction']['signatures'][0])
             #print(transaction['transaction']['message']['accountKeys'][12]['pubkey'])
-        exceptThrown=False
-        try:
-            df = get_NFT_metadata(transaction['transaction']['message']['accountKeys'][12]['pubkey'])
-        except:
-            exceptThrown=True
-            pass
+        exceptThrown=True
+        df2 = parseLiquidationLot(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
+        time.sleep(0.5)
+        df = pd.DataFrame({"nftMint":"0"}, index=[0])
+        if df2['lotState'].to_string(index=False) == "active":
+            try:
+                df = get_NFT_metadata(transaction['transaction']['message']['accountKeys'][12]['pubkey'])
+                time.sleep(2.5)
+                exceptThrown=False
+            except:
+                pass
             #print(df)
             #print(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
-        df2 = parseLiquidationLot(transaction['transaction']['message']['accountKeys'][1]['pubkey'])
         df3 = pd.merge(df, df2, how="inner", left_on='nftMint', right_on='nftMint')
         if exceptThrown:
             dfAll = pd.concat([dfAll, df2], ignore_index=True, copy=True)
         else:
             dfAll = pd.concat([dfAll, df3], ignore_index=True, copy=True)
-    print(dfAll[["name", "lotState", "ticketsCount", "liquidationLot"]])
-
+    if 'name' in df.columns:
+        print(dfAll[["name", "lotState", "ticketsCount", "liquidationLot"]])
+    else:
+        print(dfAll[["lotState", "ticketsCount", "liquidationLot"]])
+    dfAll.to_csv("./temp/output.csv")
 
 if __name__ == "__main__":
     asyncio.run(main())
