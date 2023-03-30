ef5667686e433321b37bf8094a0f10ce6e7ad844
Damis <adam.smehyl@seznam.cz>
2023-03-30 20:10:12 +0200

    frontend update - more buttons

--- code/escrow-headon-attempt/app/src/App.js
+++ code/escrow-headon-attempt/app/src/App.js
@@ -1,14 +1,22 @@
+import { Buffer } from 'buffer';
+
 import './App.css';
 import {useState} from "react";
 import {Connection, PublicKey} from '@solana/web3.js';
-// import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
+
+import {LAMPORTS_PER_SOL, SYSVAR_RENT_PUBKEY} from "@solana/web3.js";
+import * as splToken from "@solana/spl-token";
 import {Program, AnchorProvider, web3} from '@coral-xyz/anchor';
 import idl from "./idl.json"; 
+
+
 
 import {PhantomWalletAdapter, SolflareWalletAdapter} from '@solana/wallet-adapter-wallets';
 import {useWallet, WalletProvider, ConnectionProvider} from '@solana/wallet-adapter-react';
 import {WalletModalProvider, WalletMultiButton} from '@solana/wallet-adapter-react-ui';
 require('@solana/wallet-adapter-react-ui/styles.css');
+
+window.Buffer = Buffer;
 
 const wallets = [
   new PhantomWalletAdapter(),
@@ -18,9 +26,10 @@ const wallets = [
 const {Keypair, SystemProgram} = web3;
 
 const programId = new PublicKey(idl.metadata.address);
+let token_mint;
 
 const opts = {
-  preflightCommitment: "Processed"
+  preflightCommitment: "confirmed"
 }
 
 const network = "http://127.0.0.1:8899"
@@ -34,6 +43,73 @@ const Escrow = () => {
     const provider = new AnchorProvider(connection, wallet, opts.preflightCommitment)
     return provider;
   }
+
+  const requestSOLairdrop = async () => {
+    const provider = await getProvider();
+    const program = new Program(idl, programId, provider);
+
+    setMessage("Airdropping $SOL.");
+    try {
+      await provider.connection.requestAirdrop(wallet.publicKey, 1 * LAMPORTS_PER_SOL);
+      setMessage("$SOL airdropped.");
+    } catch (error) {
+      setMessage(`Error airdropping $SOL: ${error.message}`);
+    }
+  };
+
+  const createToken = async () => {
+    const provider = await getProvider();
+    const program = new Program(idl, programId, provider);
+
+    setMessage("Creating a new token.");
+    try {
+
+      token_mint = new PublicKey();
+      const createMintInstruction = splToken.createInitializeMintInstruction(
+        token_mint,
+        6,
+        provider.wallet.publicKey,
+        provider.wallet.publicKey,
+      );
+
+      const transaction = new web3.Transaction().add(createMintInstruction);
+      await provider.wallet.signTransaction(transaction);
+      const signature = await provider.connection.sendTransaction(transaction);
+      await provider.connection.confirmTransaction(signature, opts.preflightCommitment);
+      //...
+      setMessage("Token created.");
+    } catch (error) {
+      setMessage(`Error creating a new token: ${error.message}`);
+    }
+  };
+
+  const createUserAccount = async () => {
+    const provider = await getProvider();
+    const program = new Program(idl, programId, provider);
+
+    setMessage("Creating an user token account.");
+    try {
+      // Add logic 
+
+      setMessage("User token account created.");
+    } catch (error) {
+      setMessage(`Error creating a new token account: ${error.message}`);
+    }
+  };
+
+  const airdropToken = async () => {
+    const provider = await getProvider();
+    const program = new Program(idl, programId, provider);
+
+    setMessage("Airdropping token to the user token account...");
+    try {
+      // Add logic 
+
+      setMessage("Token airdropped.");
+    } catch (error) {
+      setMessage(`Error airdropping tokens: ${error.message}`);
+    }
+  };
 
   const initializeEscrow = async () => {
     const provider = await getProvider();
@@ -76,6 +152,10 @@ const Escrow = () => {
     return (
       <div className='Escrow'>
         <div>
+          <button onClick={requestSOLairdrop}>Airdrop $SOL to user wallet</button>
+          <button onClick={createToken}>Create a new token</button>
+          <button onClick={createUserAccount}>Create user token account</button>
+          <button onClick={airdropToken}>Mint token to the account</button>
           <button onClick={initializeEscrow}>Initialize Escrow</button>
           <button onClick={retrieveFromEscrow}>Retrieve Tokens</button>
           <p>{message}</p>
